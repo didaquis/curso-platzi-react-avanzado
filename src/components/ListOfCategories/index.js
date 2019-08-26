e8f98f54c665dd06c647e5854f9f4bd47d409998
Dídac García <didaquis@gmail.com>
2019-08-26 19:48:23 +0200

    Styles has been improved

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 import { Category } from '../Category'
 
@@ -6,6 +6,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = ( ) => {
 	const [categories, setCategories] = useState([])
+	const [showFixed, setShowFixed] = useState(false)
 
 	useEffect(function () {
 		window.fetch('https://curso-platzi-react-avanzado.didaquis.now.sh/categories')
@@ -13,13 +14,34 @@ export const ListOfCategories = ( ) => {
 			.then(response => {
 				setCategories(response)
 			})
-	}, [])
+	}, []) /* Remember: if you don't use second parameter of useEffect, the useEffect can force a loop of rendering! */
 
-	return (
-		<List>
+	useEffect(function () {
+		const onScroll = () => {
+			const isShowFixed = window.scrollY > 200
+			setShowFixed(isShowFixed)
+		}
+
+		document.addEventListener('scroll', onScroll)
+
+		/* useEffect can return a function. In this case, we are removing a DOM listener to prevent a memory leak */
+		return function () {
+			document.removeEventListener('scroll', onScroll)
+		}
+	}, [showFixed])
+
+	const renderList = (fixed) => (
+		<List className={ fixed ? 'fixed' : '' }>
 			{
 				categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
 			}
 		</List>
+	)
+
+	return (
+		<Fragment>
+			{ showFixed && renderList(true) }
+			{ renderList() }
+		</Fragment>
 	)
 }
--- src/styles/animation.js
+++ src/styles/animation.js
@@ -1,6 +1,5 @@
 import { css, keyframes } from 'styled-components'
 
-
 const fadeInKeyFrames = keyframes`
 	from {
 		filter: blur(5px);
@@ -13,4 +12,17 @@ const fadeInKeyFrames = keyframes`
 	}
 `
 
+const fadeOutKeyFrames = keyframes`
+	from {
+		filter: blur(0);
+		opacity: 1;
+	}
+
+	to {
+		filter: blur(5px);
+		opacity: 0;
+	}
+`
+
 export const fadeIn = ( { time = '1s', type = 'ease' } = {}) => css`animation: ${time} ${fadeInKeyFrames} ${type};`
+export const fadeOut = ( { time = '1s', type = 'ease' } = {}) => css`animation: ${time} ${fadeOutKeyFrames} ${type};`
