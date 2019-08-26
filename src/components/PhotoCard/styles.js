d7332aa2523f2f7f32219fbcd76114872ab7b67b
Dídac García <didaquis@gmail.com>
2019-08-26 17:32:36 +0200

    Moved global styles to an specific folder

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,16 +1,5 @@
-import styled, { keyframes } from 'styled-components'
-
-const fadeInKeyFrames = keyframes`
-	from {
-		filter: blur(5px);
-		opacity: 0;
-	}
-
-	to {
-		filter: blur(0);
-		opacity: 1;
-	}
-`
+import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
 	border-radius: 10px;
@@ -23,7 +12,7 @@ export const ImgWrapper = styled.div`
 `
 
 export const Image = styled.img`
-	animation: 1s ${fadeInKeyFrames} ease;
+	${fadeIn()};
 	box-shadow: 0px 10px 14px rgba(0, 0, 0, .2);
 	height: 100%;
 	object-fit: cover;
