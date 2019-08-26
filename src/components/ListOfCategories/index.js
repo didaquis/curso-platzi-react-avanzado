33b8e54f8401f940c3a079a93e007777fe6ff7c6
Dídac García <didaquis@gmail.com>
2019-08-26 20:04:39 +0200

    Refactored code

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -31,7 +31,7 @@ export const ListOfCategories = ( ) => {
 	}, [showFixed])
 
 	const renderList = (fixed) => (
-		<List className={ fixed ? 'fixed' : '' }>
+		<List fixed={fixed}>
 			{
 				categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
 			}
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { fadeIn } from '../../styles/animation'
 
 export const List = styled.ul`
@@ -6,7 +6,7 @@ export const List = styled.ul`
 	overflow: hidden;
 	width: 100%;
 
-	&.fixed {
+	${ props => props.fixed && css`
 		background: #fff;
 		border-radius: 60px;
 		box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
@@ -20,7 +20,7 @@ export const List = styled.ul`
 		transform: scale(0.5);
 		z-index: 1;
 		${fadeIn()};
-	}
+	`}
 `
 
 export const Item = styled.li`
