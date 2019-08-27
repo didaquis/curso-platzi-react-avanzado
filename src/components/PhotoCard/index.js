2d204d9078ea24a6b735ee85206568c88b7f1e7d
Dídac García <didaquis@gmail.com>
2019-08-27 17:47:34 +0200

    Avoid rendering components out of viewport

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 
-import { ImgWrapper, Image, Button } from './styles'
+import { ImgWrapper, Image, Button, Article } from './styles'
 
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
-	return (
-		<article>
-			<a href={`/detail/${id}`}>
-				<ImgWrapper>
-					<Image src={src}/>
-				</ImgWrapper>
-			</a>
 
-			<Button>
-				<MdFavoriteBorder size='32px' /> {likes} likes!
-			</Button>
-		</article>
+	/* Podemos saber que elemento está en el viewport y cual no. 
+	 * Esto permite evitar renderizar elementos que no se van a visualizar
+	 */
+	const [show, setShow] = useState(false)
+
+	const element = useRef(null)
+	useEffect(function () {
+		const observer = new window.IntersectionObserver(function (entries) {
+			const { isIntersecting } = entries[0]
+			if (isIntersecting) {
+				setShow(true)
+				observer.disconnect()
+			}
+		})
+
+		observer.observe(element.current)
+	}, [element])
+
+	return (
+		<Article ref={element}>
+			{
+				show && <Fragment>
+					<a href={`/detail/${id}`}>
+						<ImgWrapper>
+							<Image src={src}/>
+						</ImgWrapper>
+					</a>
+
+					<Button>
+						<MdFavoriteBorder size='32px' /> {likes} likes!
+					</Button>
+				</Fragment>
+			}
+		</Article>
 	)
 }
