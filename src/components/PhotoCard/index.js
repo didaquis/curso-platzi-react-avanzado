c9c67982385ee5d679c3dac44fed326321233ed0
Dídac García <didaquis@gmail.com>
2019-09-06 00:04:13 +0200

    Added basic logic of like button

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useRef, useState } from 'react'
 
 import { ImgWrapper, Image, Button, Article } from './styles'
 
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -12,6 +12,17 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 	 * Esto permite evitar renderizar elementos que no se van a visualizar
 	 */
 	const [show, setShow] = useState(false)
+
+	const key = `like-${id}`
+	const [liked, setLiked] = useState(() => {
+		try {
+			const like = window.localStorage.getItem(key)
+			return like
+		} catch (e) {
+			return false
+		}
+	})
+
 
 	const element = useRef(null)
 	useEffect(function () {
@@ -32,6 +43,18 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 		})
 	}, [element])
 
+
+	const Icon = liked ? MdFavorite : MdFavoriteBorder /* Es importante que la primera letra de la constante esté en mayúscula para que React la interprete como un componente durante el renderizado */
+
+	const setLocalStorage = value => {
+		try {
+			window.localStorage.setItem(key, value)
+			setLiked(value)
+		} catch (e) {
+			console.error(e)
+		}
+	}
+
 	return (
 		<Article ref={element}>
 			{
@@ -42,8 +65,8 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE } ) => {
 						</ImgWrapper>
 					</a>
 
-					<Button>
-						<MdFavoriteBorder size='32px' /> {likes} likes!
+					<Button onClick={() => setLocalStorage(!liked)}>
+						<Icon size='32px' /> {likes} likes!
 					</Button>
 				</Fragment>
 			}
