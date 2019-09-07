bf6e846f96b101c1fba3aff97b07d73ea49d89ae
Dídac García <didaquis@gmail.com>
2019-09-07 21:23:45 +0200

    Added Apollo client

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
-
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = ( ) => {
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const withPhotos = graphql(gql`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
 	return (
 		<ul>
 			{
-				[1, 2, 3, 4, 5, 6].map(id => <PhotoCard key={id.id} id={id} {...id} />)
+				photos.map(photo => <PhotoCard key={photo.id} {...photo} />)
 			}
 		</ul>
 	)
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import { App } from './App'
 
+const client = new ApolloClient({
+	uri: 'https://curso-platzi-react-avanzado.didaquis.now.sh/graphql'
+})
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>, document.getElementById('app'))
