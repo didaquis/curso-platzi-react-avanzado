1676040d64f68f963c4f9fa47c12e54239ef83c4
Dídac García <didaquis@gmail.com>
2019-10-20 21:49:35 +0200

    Form has been refactored

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
+
+import { useInputValue } from '../../hooks/useInputValue'
 
 
 export const UserForm = ({ onSubmit }) => {
 
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+	const email = useInputValue('')
+	const password = useInputValue('')
 
 	return (
 		<form onSubmit={onSubmit}>
-			<input placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} required autoFocus />
-			<input placeholder='Contraseña' type='password' value={password} onChange={e => setPassword(e.target.value)} required />
+			<input placeholder='Email' {...email} required autoFocus />
+			<input placeholder='Contraseña' type='password' {...password} required />
 			<button>Iniciar sesión</button>
 		</form>
 	)
