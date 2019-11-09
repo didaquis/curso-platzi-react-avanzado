50494ff091647d9c7c7a9f70903d4c54b443b494
Dídac García <didaquis@gmail.com>
2019-11-09 11:22:17 +0100

    Add more Cypress test

--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -8,13 +8,21 @@ describe('Petgram', () => {
 
 	it('permite navegar a una categoría y ver fotografías', () => {
 		cy.visit('/pet/1')
+		cy.wait(25)
 		cy.get('article')
 	})
 
 	it('permite navegar mediante navbar hasta la "home" de la app', () => {
 		cy.visit('/pet/1')
+		cy.wait(25)
 		cy.get('nav a').first().click()
 		cy.url().should('include', '/')
+	})
+
+	it('muestra el formulario de registro e inicio de sesión a los usuarios no autenticados cuando navegan a "favs"', () => {
+		cy.visit('/favs')
+		cy.wait(25)
+		cy.get('form').should('have.length', 2)
 	})
 
 })
